8a836d4691ed631bdb12b4822c233390b3794c4b
Tahiya07 <tahiyazareen2020@gmail.com>
2025-05-01 15:16:50 +0600

    v 0.0.1 - Tahiya --copied project of uap_cse

---
+++ uap_cse/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def undergraduate(request):
+    return render(request, 'hard_html/undergraduate.html')
+
+def graduate(request):
+    return render(request, 'hard_html/graduate.html')
+
+def tuition(request):
+    return render(request, 'hard_html/tuition.html')
+
+def why_cse(request):
+    return render(request, 'hard_html/why_cse.html')
+
+def host(request):
+    return render(request, 'hard_html/icpc_host.html')
+
+def clubs(request):
+    return render(request, 'hard_html/clubs.html')
+
+def gallery(request):
+    return render(request, 'hard_html/gallery.html')
+
+
+
